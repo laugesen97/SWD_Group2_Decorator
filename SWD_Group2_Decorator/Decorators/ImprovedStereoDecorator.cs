e53b64f6a7c493548729a168970bf59102c53354
Frederik Damgaard Laugesen <au651033@uni.au.dk>
2022-03-28 13:29:24 +0200

    Tilføjet stereoanlæg og subwoofer i bagagerummet så Nanna blev tilfreds

---
+++ SWD_Group2_Decorator/Decorators/ImprovedStereoDecorator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWD_Group2_Decorator.Decorators
+{
+    class ImprovedStereoDecorator : Decorator
+    {
+        double _addedPrice;
+        public ImprovedStereoDecorator(Car car, double AddedPrice) : base(car)
+        {
+            _addedPrice = AddedPrice;
+        }
+        public override double calculateTopSpeed()
+        {
+            return base.calculateTopSpeed();
+        }
+
+        public override string getCarModel()
+        {
+            return base.getCarModel() + " med forbedret stereoanlæg og subwoofer i bagagerummet";
+        }
+        public override double getCost()
+        {
+            return base.getCost() + _addedPrice;
+        }
+    }
+}
--- SWD_Group2_Decorator/Program.cs
+++ SWD_Group2_Decorator/Program.cs
@@ -1,15 +1,21 @@
 ﻿using SWD_Group2_Decorator;
+using SWD_Group2_Decorator.Decorators;
 
 MercedesCLA Mercedes = new MercedesCLA() { HorsePower = 20, Model = "Mercedes CLA", Cost = 2200000};
 string test = Mercedes.getCarModel();
 double cost = Mercedes.getCost();
+double test3 = Mercedes.calculateTopSpeed();
 
 SeatHeatingDecorator seatDecorator = new SeatHeatingDecorator(Mercedes, 5000);
 string test2 = seatDecorator.getCarModel();
 double cost2 = seatDecorator.getCost();
 
-double test3 = Mercedes.calculateTopSpeed();
-AddTireType tireTypeDecorator = new AddTireType(Mercedes, "soft");
+AddTireType tireTypeDecorator = new AddTireType(seatDecorator, "soft");
 double test4 = tireTypeDecorator.calculateTopSpeed();
 string test5 = tireTypeDecorator.getCarModel();
+
+ImprovedStereoDecorator stereoDecorator = new ImprovedStereoDecorator(tireTypeDecorator, 12000);
+double cost5 = stereoDecorator.getCost();
+string cost6 = stereoDecorator.getCarModel();
+
 Console.WriteLine("Done");
